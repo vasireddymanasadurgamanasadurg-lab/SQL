cad76d4c7979ca118e2f3a47d50e614fbf541a64
vasireddymanasadurgamanasadurg-lab <vasireddymanasadurgamanasadurg@gmail.com>
2026-01-01 18:06:17 +0530

    Add files via upload

---
+++ MANASA__S.sql
@@ -0,0 +1,19 @@
+create table manasa(
+sno int,
+empid int,
+surname char(1),
+name varchar(30),
+dob date,
+phone int,
+salary decimal(10),
+mobile bigint,
+bonus float,
+percentage double
+);
+
+select * from manasa;
+
+insert into manasa values(1,578,'v','DURGA','2004-06-27',123496789,45.000,9391856486,7.970000000080,8.97750000531);
+
+select * from manasa;
+
